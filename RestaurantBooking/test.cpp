5ca16b3996ce75bfbcde36093c7e38f93ab947af
ehju <eh13.ju@samsung.com>
2025-07-24 13:22:29 +0900

    [test] step6. TestDouble unittest

--- RestaurantBooking/test.cpp
+++ RestaurantBooking/test.cpp
@@ -1,6 +1,6 @@
 #include "gmock/gmock.h"
 #include "booking_scheduler.cpp"
-
+#include "testable_sms_sender.cpp"
 using namespace testing;
 
 class BookingItem : public Test {
@@ -81,7 +81,14 @@ TEST_F(BookingItem, 시간대별인원제한이있다같은시간대가다르면Capacity차있어도스케
 }
 
 TEST_F(BookingItem, 예약완료시SMS는무조건발송) {
+	TestableSmsSender testableSmsSender;
+	//arrange
+	Schedule* schedule = new Schedule{ ON_THE_HOUR, CAPACITY_PER_HOUR, CUSTOMER };
+	bookingScheduler.setSmsSender(&testableSmsSender);
+	//act
+	bookingScheduler.addSchedule(schedule); 
 
+	EXPECT_EQ(true, testableSmsSender.isSendMethodIsCalled());
 }
 
 TEST_F(BookingItem, 이메일이없는경우에는이메일미발송) {
