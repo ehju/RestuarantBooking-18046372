bfe2bdef0849d286453613e25f71eb046996dac4
ehju <eh13.ju@samsung.com>
2025-07-24 13:04:50 +0900

    [test] step 4

--- RestaurantBooking/test.cpp
+++ RestaurantBooking/test.cpp
@@ -46,6 +46,21 @@ TEST_F(BookingItem, 예약은정시에만가능하다정시인경우예약가능) {
 
 TEST_F(BookingItem, 시간대별인원제한이있다같은시간대에Capacity초과할경우예외발생) {
 
+	//arrange
+	//precondition : full booking
+	Schedule* schedule = new Schedule{ ON_THE_HOUR, CAPACITY_PER_HOUR, CUSTOMER }; 
+	bookingScheduler.addSchedule(schedule);
+	//act
+	try {
+		Schedule* schedule = new Schedule{ ON_THE_HOUR, UNDER_CAPACITY, CUSTOMER };
+		bookingScheduler.addSchedule(schedule); // exception
+		FAIL(); // not arrivable
+	}
+	catch (std::runtime_error& e){
+		//assert
+		EXPECT_EQ(string{ e.what() }, string{"Number of people is over restaurant capacity per hour"});
+	}
+	
 }
 
 TEST_F(BookingItem, 시간대별인원제한이있다같은시간대가다르면Capacity차있어도스케쥴추가성공) {
